b5a2494b73eb97506560e6b6293af41f6325dbbb
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-09 21:45:18 +0800

    Passed test
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	got := helloWorld()
-	want := "Hello random"
+	want := "Hello World"
 
 	if got != want {
 		t.Errorf("expects to be: %s, but got: %s instead.", want, got)
