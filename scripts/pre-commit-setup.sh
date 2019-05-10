ea05dcd051b8d0379c616dea3e21f7097f13fc8a
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-10 20:30:25 +0800

    Added pre-commit script
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

---
+++ scripts/pre-commit-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ''
+
+# Create the file inside .git/pre-commit
+cat ./scripts/pre-commit.sh >> .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+
+# Init message
+echo $(date) '[ Git Hooks ] Symlinking commit hook scripts...'
+
+# Check if file already symlinked
+if [ -h .git/hooks/pre-commit ]; then
+  echo $(date) '[ Git Hooks ] Commit hook scripts already symlinked'
+else
+  ln -s $(pwd)/script/pre-commit.sh .git/hooks/pre-commit
+  echo $(date) '[ Git Hooks ] Already symlinked. DONE!'
+fi
+
