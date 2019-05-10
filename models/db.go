235e9fc1bdbe4c542c7688570b401000cfce8983
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2019-05-10 19:13:03 +0800

    Update init

--- models/db.go
+++ models/db.go
@@ -1,11 +1,15 @@
 package models
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 var dbhost = getenvWithDefault("DB_HOST", "127.0.0.1")
 
 func init() {
 	// Initialize connection to db here
+	log.Printf("db host is: %s\n", dbhost)
 }
 
 func getenvWithDefault(key string, def string) string {
