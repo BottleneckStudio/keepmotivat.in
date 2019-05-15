725282335d5a0dd5dd7e1856593ac701f0e1c38c
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-15 21:42:09 +0800

    Fixed template rendering
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- template/template.go
+++ template/template.go
@@ -14,6 +14,24 @@ const (
 	ext = ".html"
 )
 
+// ParseTemplateDir parses a given directory to a template.
+func ParseTemplateDir(dir string) (*template.Template, error) {
+	var paths []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return template.ParseFiles(paths...)
+}
+
 // Template ...
 type Template struct {
 	templates map[string]*template.Template
--- template/template_test.go
+++ template/template_test.go
@@ -7,17 +7,39 @@ import (
 	temp "github.com/BottleneckStudio/keepmotivat.in/template"
 )
 
-func TestRenderTemplate(t *testing.T) {
-	tmpl := temp.New("../app/views/")
+// func TestRenderTemplate(t *testing.T) {
+// 	tmpl := temp.New("../app/views/")
+//
+// 	helloWorld := "Hello World"
+//
+// 	if err := tmpl.Render(os.Stdout, "index.html", helloWorld); err != nil {
+// 		t.Errorf("Must be nil, but got: %v.", err)
+// 	}
+//
+// 	if tmpl == nil {
+// 		t.Errorf("Template must not be nil. Got: %v instead.", tmpl)
+// 		return
+// 	}
+// }
 
-	helloWorld := "Hello World"
-
-	if err := tmpl.Render(os.Stdout, "index.html", helloWorld); err != nil {
-		t.Errorf("Must be nil, but got: %v.", err)
+func TestParseTemplateDir(t *testing.T) {
+	type TemplateData struct {
+		SITENAME string
+		SITEURL  string
 	}
 
-	if tmpl == nil {
-		t.Errorf("Template must not be nil. Got: %v instead.", tmpl)
-		return
+	data := TemplateData{
+		SITENAME: "Template inheritance",
+		SITEURL:  "https://github.com/rbo13",
 	}
+
+	tmpl, err := temp.ParseTemplateDir("../app/views")
+	if err != nil {
+		t.Error(err)
+	}
+	err = tmpl.ExecuteTemplate(os.Stdout, "base.html", &data)
+	if err != nil {
+		t.Error(err)
+	}
+
 }
