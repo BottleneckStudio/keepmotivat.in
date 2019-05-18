74bde2b08638772e5369524b81d529b7e209ec59
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-18 09:21:09 +0800

    Fixed test benchmarks
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- template/template_test.go
+++ template/template_test.go
@@ -20,13 +20,24 @@ func TestParseTemplateDir(t *testing.T) {
 		URL:      "https://github.com/rbo13",
 	}
 
-	tmpl := temp.New("../app/views")
+	tmpl := temp.New("../app/views/")
 
 	if tmpl == nil {
 		t.Fatalf("Creating instance of template should not be nil, but got an error: [ %v ] instead.", tmpl)
 	}
 
-	if err := tmpl.Render(os.Stdout, "base.html", &data); err != nil {
+	if err := tmpl.Render(os.Stdout, "hello/hello", &data); err != nil {
 		t.Fatalf("Expects to render an html output, but got an error: [ %v ] instead.", err)
+	}
+}
+
+func BenchmarkRender(b *testing.B) {
+	b.ReportAllocs()
+	tmpl := temp.New("../app/views/")
+
+	for i := 0; i < b.N; i++ {
+		if err := tmpl.Render(os.Stdout, "hello/hello", nil); err != nil {
+			panic(err)
+		}
 	}
 }
