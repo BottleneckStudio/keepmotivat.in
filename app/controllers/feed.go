b9d25530f45921b022150a20c3ec644ef11ecb60
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-27 09:27:17 +0800

    Removed /hello route
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -1,13 +1,19 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	tmpl "github.com/BottleneckStudio/keepmotivat.in/template"
 )
 
 // FeedController is the main screen
 func FeedController() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Feed")
+
+		tpl := tmpl.New("./app/views/")
+
+		if err := tpl.RenderHTML(w, "feed.html", "HELLO WORLD ALL CAPS!"); err != nil {
+			return
+		}
 	}
 }
