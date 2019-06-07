71924b293e40a58b776a65aa8c6a06ff8fa8ad1d
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-06-07 14:19:32 +0800

    Handled error
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -3,12 +3,15 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/BottleneckStudio/keepmotivat.in/app/session"
 	tmpl "github.com/BottleneckStudio/keepmotivat.in/template"
 )
 
 // FeedController is the main screen
 func FeedController() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		session.SetFlash(w, r, "error", "Some error message")
 
 		tpl := tmpl.New("./app/views/")
 
--- app/controllers/tos.go
+++ app/controllers/tos.go
@@ -3,11 +3,18 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/BottleneckStudio/keepmotivat.in/app/session"
 )
 
 // TermsOfServiceController is the TermsOfService screen
 func TermsOfServiceController() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "TermsOfService Controller")
+
+		flash := session.GetFlash(w, r)
+
+		// horray!
+		fmt.Fprintln(w, flash.Value)
 	}
 }
