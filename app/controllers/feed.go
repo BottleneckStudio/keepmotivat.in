e3af3b7d305e72e8eee0dfe76600e1f51f283389
John Kennedy Bicbic <jkbicbic@gmail.com>
2019-10-26 16:10:07 +0800

    modified controller and view for post

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -1,14 +1,17 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"BottleneckStudio/keepmotivat.in/app/session"
+	"BottleneckStudio/keepmotivat.in/models"
 	tmpl "BottleneckStudio/keepmotivat.in/template"
+	"BottleneckStudio/keepmotivat.in/usecase"
 )
 
 // FeedController is the main screen
-func FeedController() http.HandlerFunc {
+func FeedController(db *models.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		session.SetFlash(w, r, "error", "Some error message")
@@ -17,6 +20,18 @@ func FeedController() http.HandlerFunc {
 
 		data := map[string]interface{}{}
 		data["message"] = "Another World"
+
+		// Get all post ...
+		postRepo := models.NewDBPostRepository(db)
+		postUsecase := usecase.NewGetPostUsecase(postRepo)
+		posts, err := postUsecase.GetAllPosts(&models.Query{})
+		log.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@")
+		log.Println(posts)
+		if err != nil {
+			log.Println(err)
+		}
+
+		data["posts"] = posts
 
 		if err := tpl.RenderHTML(w, "feed.html", data); err != nil {
 			return
